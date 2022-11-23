57d0facbe036dd279fa909606e052676ed0f440e
rajk75 <rajkotak75@icloud.com>
2022-11-23 18:56:58 +0000

    new added

--- todo.py
+++ todo.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app= Flask(__name__)
+
 @app.route("/")
 def hello():
-    return "hello world"
+    return render_template('home.html')
+
+@app.route('/addTodo', methods=['POST'])
+def addTodo():
+     todoItem = request.form['todoItem']
+     print(todoItem)
+     return (todoItem )
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
