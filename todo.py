d477f022c454d499b2e3ebd0f8a9e09544be2bc6
rajk75 <rajkotak75@icloud.com>
2022-11-23 20:06:50 +0000

    partially finished

--- todo.py
+++ todo.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request
 app= Flask(__name__)
-
+todoList=[]
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', todoList=todoList)
 
 @app.route('/addTodo', methods=['POST'])
 def addTodo():
-     todoItem = request.form['todoItem']
-     print(todoItem)
-     return (todoItem )
+     todoItem = request.form['text1']
+     todoList.append(todoItem)
+     return render_template('home.html', todoList=todoList)
+
 if __name__=="__main__":
     app.run(debug=True)
